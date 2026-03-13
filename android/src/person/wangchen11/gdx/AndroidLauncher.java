337932bda397e3f445e6c4b19790dda2bf7cf7c5
wangchao1 <wangchao1@avit.com.cn>
2026-03-13 18:22:36 +0800

    修复android编译失败

--- android/src/person/wangchen11/gdx/AndroidLauncher.java
+++ android/src/person/wangchen11/gdx/AndroidLauncher.java
@@ -4,13 +4,13 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import person.wangchen11.gdx.game.MainGame;
+import person.wangchen11.planet.game.PlanetGame;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new MainGame(), config);
+		initialize(new PlanetGame(), config);
 	}
 }
